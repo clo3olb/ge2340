0720d89b7b156b7b6dcb35eff7d7f5c4fdc2159e
clo3olb <clo3olb@gmail.com>
2023-11-22 17:10:04 +0800

    basic formatting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,27 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
     <main>
-      <h1>GE2340 Project</h1>
+      <article>
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+          eligendi ratione accusantium ullam eius, ipsum ad. Placeat, iure.
+          Nulla recusandae vero quidem consequatur, ullam sed voluptatum.
+          Perspiciatis provident laudantium iure!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+          eligendi ratione accusantium ullam eius, ipsum ad. Placeat, iure.
+          Nulla recusandae vero quidem consequatur, ullam sed voluptatum.
+          Perspiciatis provident laudantium iure!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+          eligendi ratione accusantium ullam eius, ipsum ad. Placeat, iure.
+          Nulla recusandae vero quidem consequatur, ullam sed voluptatum.
+          Perspiciatis provident laudantium iure!
+        </p>
+      </article>
     </main>
   );
 }
